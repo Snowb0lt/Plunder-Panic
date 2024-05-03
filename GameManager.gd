3d70de0095a9fd031410292aa14809612e6ff2ed
Snowb0lt <129245958+Snowb0lt@users.noreply.github.com>
2024-05-03 07:20:44 -0700

    Added coin scoring and bouncing

--- GameManager.gd
+++ GameManager.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
 var score = 0
-
+var scoreLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	scoreLabel = $UI/ScoreText
 	print(score)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func updateScore():
+	scoreLabel.text = "Score: " + str(score)
+
+func _on_score_area_treasure_body_entered(body):
+	if body.is_in_group("Coin"):
+		score += 100
+		updateScore()
+
+
+#spawning the gold coins
+var coins = preload("res://coin.tscn")
