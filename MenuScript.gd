aded6e6f02ea867b7291c74ace4e7d832974a9b9
Snowb0lt <129245958+Snowb0lt@users.noreply.github.com>
2024-05-27 17:02:39 -0700

    Shortened the instructions and made a flipbook

--- MenuScript.gd
+++ MenuScript.gd
@@ -21,8 +21,45 @@ func _on_quit_pressed():
 func _on_information_pressed():
 	get_node("Instructions").visible = true
 	get_node("Menu Options").visible = false
+	TutorSlide3.visible = false
+	TutorSlide2.visible = false
+	TutorSlide1.visible = true
+	slideNumber = 1
 
 
 func _on_button_pressed():
 	get_node("Instructions").visible = false
 	get_node("Menu Options").visible = true
+
+var slideNumber
+@onready var TutorSlide1 = get_node("Instructions/Instruction Text PT1")
+@onready var TutorSlide2 = get_node("Instructions/Instruction Text PT2")
+@onready var TutorSlide3 = get_node("Instructions/Instruction Text PT3")
+func _on_next_button_pressed():
+	if TutorSlide1.visible:
+		TutorSlide1.visible = false
+		TutorSlide2.visible = true
+		return
+	if TutorSlide2.visible:
+		TutorSlide2.visible = false
+		TutorSlide3.visible = true
+		return
+	if TutorSlide3.visible:
+		TutorSlide3.visible = false
+		TutorSlide1.visible = true
+		return
+
+
+func _on_stop_button_pressed():
+	if TutorSlide1.visible:
+		TutorSlide1.visible = false
+		TutorSlide3.visible = true
+		return
+	if TutorSlide2.visible:
+		TutorSlide2.visible = false
+		TutorSlide1.visible = true
+		return
+	if TutorSlide3.visible:
+		TutorSlide3.visible = false
+		TutorSlide2.visible = true
+		return
